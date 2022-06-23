5926fa4abc51e3a27a6804eafa28f9cd9c376eba
pierrek6e <pierrecass75@gmail.com>
2022-06-23 18:43:06 +0700

    add banner and product components

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ import { Product, HeroBanner, Footer } from "../components";
 const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {products?.map((product, i) => (
-          <div key={i}>{product.name}</div>
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
         ))}
       </div>
       <Footer />
