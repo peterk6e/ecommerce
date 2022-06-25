c99f83e058c9fd686d9ecbfa61e2a65069605bab
pierrek6e <pierrecass75@gmail.com>
2022-06-25 10:51:20 +0700

    Add footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import {
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiFillFacebook,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer-container">
+      <p>2022 J&M Headphones | All Rights Reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiFillFacebook />
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
