834748b60e5b5f2e50dac4991f8016d1ef58babc
pierrek6e <pierrecass75@gmail.com>
2022-06-26 16:55:57 +0700

    minor changes

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,7 +7,6 @@ import {
 } from "react-icons/ai";
 
 import { client, urlFor } from "../../lib/client";
-
 import { Product } from "../../components";
 import { useStateContext } from "../../context/StateContext";
 
@@ -48,48 +47,49 @@ const ProductDetails = ({ products, product }) => {
               />
             ))}
           </div>
-          <div className="product-detail-desc">
-            <h1>{name}</h1>
-            <div className="reviews">
-              <div>
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiOutlineStar />
-              </div>
-              <p>(20)</p>
+        </div>
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
             </div>
-            <h4>Details:</h4>
-            <p>{details}</p>
-            <p className="price">${price}</p>
-            <div className="quantity">
-              <h3>Quantity</h3>
-              <p className="quantity-desc">
-                <span className="minus" onClick={decQty}>
-                  <AiOutlineMinus />
-                </span>
-                <span className="num">{qty}</span>
-                <span className="plus" onClick={incQty}>
-                  <AiOutlinePlus />
-                </span>
-              </p>
-            </div>
-            <div className="buttons">
-              <button
-                type="button"
-                className="add-to-cart"
-                onClick={() => onAdd(product, qty)}
-              >
-                Add to Cart
-              </button>
-              <button type="button" className="buy-now" onClick={handleBuyNow}>
-                Buy Now
-              </button>
-            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick={decQty}>
+                <AiOutlineMinus />
+              </span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}>
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button
+              type="button"
+              className="add-to-cart"
+              onClick={() => onAdd(product, qty)}
+            >
+              Add to Cart
+            </button>
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
+              Buy Now
+            </button>
           </div>
         </div>
       </div>
+
       <div className="maylike-product-wrapper">
         <h2> You may also like</h2>
         <div className="marquee">
